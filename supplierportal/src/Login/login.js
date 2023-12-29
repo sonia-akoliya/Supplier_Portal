d50e960193f90528c8c373760c48450c515692c4
soniaakoliya <yuktaakoliya@gmail.com>
2023-12-29 10:47:45 +0530

    changes for login form

--- supplierportal/src/Login/login.js
+++ supplierportal/src/Login/login.js
@@ -3,6 +3,8 @@ import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from 'react-router-dom';
 import { selectLoginUser } from './login.selector';
 import { fetchLoginUser } from './login.action';
+import backgroundImage from './BackGround.png';
+import {logo} from './Logo.png';
 
 const Login = () => {
     const dispatch = useDispatch();
@@ -10,7 +12,7 @@ const Login = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const selectedLoginUser = useSelector(selectLoginUser);
-    console.log(selectedLoginUser, 'selectedLoginUserselectedLoginUser');
+
     const handleLogin = async () => {
         try {
             const queryFilter = {
@@ -25,21 +27,25 @@ const Login = () => {
     };
 
     return (
-        <div>
-            <h2>Login</h2>
-            <input
-                type="text"
-                placeholder="Username"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-            />
-            <input
-                type="password"
-                placeholder="Password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-            />
-            <button onClick={handleLogin}>Login</button>
+        <div style={{ backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover', minHeight: '100vh', display: 'flex'}}>
+            <div style={{ backgroundColor: 'rgba(255, 255, 255, 0.8)', padding: '20px', maxWidth: '400px', width: '80%' }}>
+                <h2>Login</h2>
+                <input
+                    type="text"
+                    placeholder="Username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    style={{ marginBottom: '10px' }}
+                />
+                <input
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    style={{ marginBottom: '10px' }}
+                />
+                <button onClick={handleLogin} style={{ width: '100%' }}>Login</button>
+            </div>
         </div>
     );
 };
